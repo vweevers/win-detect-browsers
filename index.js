b6aa8878fe034df295df333fa16077f25e018110
Vincent Weevers <mail@vincentweevers.nl>
2016-11-27 19:31:51 +0100

    feat: add channel info to chrome

--- index.js
+++ index.js
@@ -2,48 +2,56 @@ var Finder = require('./lib/finder')
   , xtend = require('xtend')
   , debug = require('debug')('win-detect-browsers')
   , path = require('path')
-  , regStream = require('./lib/reg-stream')
-  , merge = require('merge-stream')
-  , concat = require('concat-stream')
-  , unique = require('unique-stream')
-  , through2 = require('through2')
+  , registry = require('./lib/reg-stream')
+  , after = require('after')
+
+  // TODO: remove these deps after reg-stream is gone
+  // , merge = require('merge-stream')
+  // , concat = require('concat-stream')
+  // , unique = require('unique-stream')
+  // , through2 = require('through2')
 
 var defaults = {
   lucky: false,
   browsers: require('./lib/browsers')
 }
 
-module.exports = function (names, opts, cb) {
+module.exports = function detect (names, opts, done) {
   if (typeof names == 'string') names = [names]
-  else if (!Array.isArray(names)) cb = opts, opts = names, names = null
+  else if (!Array.isArray(names)) done = opts, opts = names, names = null
 
-  if (typeof opts == 'function') cb = opts, opts = xtend(defaults)
+  if (typeof opts == 'function') done = opts, opts = xtend(defaults)
   else opts = xtend(defaults, opts)
 
-  var browsers = opts.browsers
-    , reg = regStream()
-
-  if (!names || !names.length)
-    names = Object.keys(browsers)
-
-  var stream = merge(names.map(function (name) {
-    return new Finder(name, browsers[name], reg, opts)
-  })).pipe(unique(function (b) {
-    return b.path.toLowerCase()
-  }))
-
-  stream.on('end', reg.end.bind(reg))
-
-  if (cb) {
-    var finished = 0
-    var wrapped = function (err, browsers) {
-      if (finished++) return
-      cb(err, browsers)
-    }
-
-    stream.once('error', wrapped)
-    stream.pipe(concat(wrapped.bind(null, null)))
+  if (!names || !names.length) {
+    names = Object.keys(opts.browsers)
   }
 
-  return stream;
+  const reg = registry()
+      , result = []
+      , next = after(names.length, finish)
+
+  // For debugging: count the number
+  // of ways we found the browsers.
+  let totalMethods = 0
+
+  names.forEach(function (name) {
+    const browser = opts.browsers[name]
+    if (!browser) throw new Error('No such browser is defined: ' + name)
+
+    const f = new Finder(name, browser, reg, opts)
+
+    f.on('error', next).on('end', (res, methods) => {
+      for(let b of res.values()) result.push(b)
+      totalMethods+= methods
+      next()
+    })
+  })
+
+  function finish (err) {
+    reg.end()
+
+    if (err) done(err)
+    else done(err, result, totalMethods)
+  }
 }
