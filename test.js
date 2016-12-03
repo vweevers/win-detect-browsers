370838c0d4ba9027490b49098171bfff37775458
Vincent Weevers <mail@vincentweevers.nl>
2016-12-03 08:00:14 +0100

    tests: code style

--- test.js
+++ test.js
@@ -14,72 +14,74 @@
 
 process.stderr.setMaxListeners(100)
 
-var test = require('tape')
-  , detect = require('./')
-  , path = require('path')
-  , compareVersion = require('compare-version')
+const test = require('tape')
+    , detect = require('./')
+    , path = require('path')
+    , compareVersion = require('compare-version')
+    , wbin = require('windows-bin')
 
-var argv = require('yargs')
-  .boolean('opera')
-  .boolean('canary')
-  .boolean('phantomjs')
-  .argv;
+const argv = require('yargs')
+    .boolean('opera')
+    .boolean('canary')
+    .boolean('phantomjs')
+    .argv
 
-test('find cscript', function(t){
+test('find cscript', function (t) {
   t.plan(6)
 
-  var wbin = require('windows-bin'), async = false
+  let async = false
 
-  wbin('cscript', function(err, path){
+  wbin('cscript', function (err, path) {
     t.ok(path, 'cb 1 has path')
     t.ok(async, 'cb 1 is async')
   })
 
-  wbin('cscript', function(err, path){
+  wbin('cscript', function (err, path) {
     t.ok(path, 'cb 2 has path')
     t.ok(async, 'cb 2 is async')
 
-    var async2 = false
-    wbin('cscript', function(err, path){
+    let async2 = false
+
+    wbin('cscript', function (err, path) {
       t.ok(path, 'cb 3 has path')
       t.ok(async2, 'cb 3 is async')
     })
+
     async2 = true
   })
 
   async = true
 })
 
-test('detect all', function(t){
-  t.plan(6)
+test('detect all', function (t) {
+  t.plan(5)
 
   detect(function(err, results){
     t.ifError(err, 'no error')
 
-    var names = results.map(function(b){ return b.name })
+    const names = results.map(b => b.name)
+    const withVersions = results.filter(hasVersion).length
 
     t.ok(names.indexOf('chrome')>=0, 'found chrome')
     t.ok(names.indexOf('firefox')>=0, 'found firefox')
     t.ok(names.indexOf('ie')>=0, 'found ie')
-    t.ok(names.indexOf('opera')>=0, 'found opera')
 
-    var len = results.filter(hasVersion).length
-    t.equal(len, results.length, 'have version numbers')
+    t.equal(withVersions, results.length, 'have version numbers')
   })
 })
 
-test('detect chrome', function(t){
-  detect('chrome', function(err, results){
+test('detect chrome', function (t) {
+  detect('chrome', function (err, results) {
     t.ifError(err, 'no error')
 
-    var names = results.filter(function(b){ return b.name === 'chrome' })
+    const names = results.filter(b => b.name === 'chrome')
     t.equal(names.length, results.length, 'have names')
 
-    var versions = results.filter(hasVersion)
+    const versions = results.filter(hasVersion)
     t.equal(versions.length, results.length, 'have version numbers')
 
     if (argv.canary) {
-      var msg = 'found ' + results.length + ' chrome versions'
+      const msg = 'found ' + results.length + ' chrome versions'
       t.ok(results.length >= 2, msg)
     }
 
@@ -87,71 +89,66 @@ test('detect chrome', function(t){
   })
 })
 
-test('detect chrome and firefox', function(t){
+test('detect chrome and firefox', function (t) {
   t.plan(4)
 
-  detect(['chrome', 'firefox'], function(err, results){
+  detect(['chrome', 'firefox'], function (err, results) {
     t.ifError(err, 'no error')
 
-    var names = results.map(function(b){ return b.name })
+    const names = results.map(b => b.name)
+    const withVersions = results.filter(hasVersion).length
 
     t.ok(names.indexOf('chrome')>=0, 'found chrome')
     t.ok(names.indexOf('firefox')>=0, 'found firefox')
-
-    var len = results.filter(hasVersion).length
-    t.ok(len>=2, 'have version numbers')
+    t.ok(withVersions >= 2, 'have version numbers')
   })
 })
 
-test('detect first chrome and firefox', function(t){
+test('detect first chrome and firefox', function (t) {
   t.plan(3)
 
-  detect(['chrome', 'firefox'], {lucky: true}, function(err, results){
+  detect(['chrome', 'firefox'], { lucky: true }, function (err, results) {
     t.ifError(err, 'no error')
 
-    var names = results.map(function(b){ return b.name })
-
-    names.sort()
+    const names = results.map(b => b.name).sort()
 
     t.deepEqual(names, ['chrome', 'firefox'], 'found chrome and firefox')
     t.equal(results.filter(hasVersion).length, 2, 'have version numbers')
   })
 })
 
-;(argv.opera ? test : test.skip)('detect all opera versions', function (t) {
+maybe(argv.opera)('detect all opera versions', function (t) {
   t.plan(3)
 
-  detect('opera', function(err, results){
+  detect('opera', function (err, results) {
     t.ifError(err, 'no error')
     t.equal(results.length, 3)
 
-    var versions = results.map(function(b){ return b.version })
-    var uniq = versions.filter(function(v, i){
-      return versions.lastIndexOf(v)===i
-    })
+    const versions = results.map(b => b.version)
+    const uniq = versions.filter((v, i) => versions.lastIndexOf(v) === i)
 
     t.equal(uniq.length, 3, 'unique versions')
   })
 })
 
-;(argv.phantomjs ? test : test.skip)('detect phantomjs', function (t) {
+maybe(argv.phantomjs)('detect phantomjs', function (t) {
   t.plan(3)
 
-  detect('phantomjs', function(err, results){
+  detect('phantomjs', function (err, results) {
     t.ifError(err, 'no error')
     t.equal(results[0].name, 'phantomjs')
     t.ok(hasVersion(results[0]), 'has version')
   })
 })
 
-test('concurrency', function(t){
-  var n = 5
+test('concurrency', function (t) {
+  const n = 5
 
   t.plan(n*3)
-  for(var i=n; i>0; i--) chrome()
+  for(let i=n; i>0; i--) chrome()
 
-  function chrome() {
-    detect('chrome', {lucky: true}, function(err, results){
+  function chrome () {
+    detect('chrome', { lucky: true }, function (err, results) {
       t.ifError(err, 'no error')
       t.equal(results.length, 1)
       t.equal(results.filter(hasVersion).length, 1)
@@ -159,8 +156,8 @@ test('concurrency', function(t){
   }
 })
 
-test('no result', function(t){
-  var browsers = {
+test('no result', function (t) {
+  const browsers = {
     beep: {
       bin: 'beep.exe',
       find: function() {
@@ -170,14 +167,15 @@ test('no result', function(t){
   }
 
   t.plan(2)
-  detect({browsers: browsers}, function(err, results){
+
+  detect({ browsers }, function (err, results) {
     t.ifError(err, 'no error')
     t.equal(results.length, 0)
   })
 })
 
-test('no result asynchronicity', function(t){
-  var browsers = {
+test('no result asynchronicity', function (t) {
+  const browsers = {
     beep: {
       bin: 'beep.exe',
       find: function() {
@@ -188,16 +186,21 @@ test('no result asynchronicity', function(t){
 
   t.plan(3)
 
-  var async = false
-  detect({browsers: browsers}, function(err, results){
+  let async = false
+
+  detect({browsers: browsers}, function (err, results) {
     t.ifError(err, 'no error')
     t.equal(results.length, 0)
     t.equal(async, true)
   })
 
-  async = true;
+  async = true
 })
 
-function hasVersion(b){
+function hasVersion (b) {
   return b.version && b.version.match(/[\d\.]+/)
+}
+
+function maybe (enable) {
+  return enable ? test : test.skip
 }
